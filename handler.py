7ce24d5750fa7f610cf4737249ff6c7aac8beb50
mt8 <info@mt8.biz>
2020-12-15 00:04:38 +0900

    fix const

--- handler.py
+++ handler.py
@@ -8,14 +8,6 @@ GA_AUTH_SCOPE    = 'https://www.googleapis.com/auth/analytics.readonly'
 GA_AUTH_KEY_PATH = 'key.json'
 GA_API_BASE_URL  = 'https://www.googleapis.com/analytics/v3/data/ga'
 
-SHIFTER_PUB_URL    = os.environ.get('SHIFTER_PUB_URL')
-GA_IDS             = os.environ.get('GA_IDS')
-GA_START_DATE      = os.environ.get('GA_START_DATE')
-GA_END_DATE        = os.environ.get('GA_END_DATE')
-GA_DIMENSION_NAME  = os.environ.get('GA_DIMENSION_NAME')
-GA_DIMENSION_VALIE = os.environ.get('GA_DIMENSION_VALIE')
-GA_MAX_RESULTS     = os.environ.get('GA_MAX_RESULTS')
-
 def main(event, context):
 
     token = ServiceAccountCredentials.from_json_keyfile_name(GA_AUTH_KEY_PATH, GA_AUTH_SCOPE).get_access_token().access_token
@@ -26,7 +18,7 @@ def main(event, context):
         return {
             'statusCode': 200,
             'headers': {
-                'Access-Control-Allow-Origin': SHIFTER_PUB_URL
+                'Access-Control-Allow-Origin': os.environ.get('SHIFTER_PUB_URL')
             },
             'body': json.dumps(res.json())
         }
@@ -34,20 +26,20 @@ def main(event, context):
         return {
             'statusCode': 500,
             'headers': {
-                'Access-Control-Allow-Origin': SHIFTER_PUB_URL
+                'Access-Control-Allow-Origin': os.environ.get('SHIFTER_PUB_URL')
             },
             'body': 'error'
         }
 
 def build_GApi_url(token):
-    url  = GA_API_BASE_URL + '?ids=' + GA_IDS
-    url += '&start-date=' + GA_START_DATE
-    url += '&end-date=' + GA_END_DATE
+    url  = GA_API_BASE_URL + '?ids=' + os.environ.get('GA_IDS')
+    url += '&start-date=' + os.environ.get('GA_START_DATE')
+    url += '&end-date=' + os.environ.get('GA_END_DATE')
     url += '&metrics=ga%3Apageviews'
     url += '&dimensions=ga%3ApagePath%2Cga%3ApageTitle'
-    url += '&filters=' + GA_DIMENSION_NAME + '%3D%3D' + GA_DIMENSION_VALIE
+    url += '&filters=' + os.environ.get('GA_DIMENSION_NAME') + '%3D%3D' + os.environ.get('GA_DIMENSION_VALUE')
     url += '&sort=-ga%3Apageviews'
     url += '&start-index=1'
-    url += '&max-results=' + GA_MAX_RESULTS
+    url += '&max-results=' + os.environ.get('GA_MAX_RESULTS')
     url += '&access_token=' + token
     return url
